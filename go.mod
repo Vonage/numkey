63718210cfc16a19343e114cd510bee39e2624ca
nicolaasuni <nicola.asuni@vonage.com>
2021-01-12 17:30:20 +0000

    Update dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nexmoinc/numkey
 
 go 1.15
+
+require github.com/golang/mock v1.4.4 // indirect
--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="numkey",
-    version="1.1.7.1",
+    version="1.1.8.1",
     keywords=("numkey E.164 shortcode lvn did encoding"),
     description="NumKey Bindings for Python",
     long_description=read("../README.md"),
