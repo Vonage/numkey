369507a493ecaf67cd2128f849966edd2348b8e2
nicolaasuni-vonage <nicola.asuni+github@vonage.com>
2023-05-26 07:29:23 +0100

    Upstream fix: c/set.h/order_uint64_t returned index

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="numkey",
-    version="1.5.9.1",
+    version="1.5.10.1",
     keywords=("numkey E.164 shortcode lvn did encoding"),
     description="NumKey Bindings for Python",
     long_description=read("../README.md"),
