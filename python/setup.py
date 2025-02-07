425a4ff25535032bcd7e54ee68eab8a1d87d62de
nicolaasuni-vonage <nicola.asuni@vonage.com>
2025-02-07 08:11:12 +0000

    Update version

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="numkey",
-    version="1.6.26.1",
+    version="1.6.27.1",
     keywords=("numkey E.164 shortcode lvn did encoding"),
     description="NumKey Bindings for Python",
     long_description=read("../README.md"),
