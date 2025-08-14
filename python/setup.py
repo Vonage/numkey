f6c4825710deebccef5eaa1b9fcba16b4f05789f
nicolaasuni-vonage <nicola.asuni@vonage.com>
2025-08-14 16:27:14 +0100

    remove gotest package

--- python/setup.py
+++ python/setup.py
@@ -30,7 +30,7 @@ class RunTests(Command):
 
 setup(
     name="numkey",
-    version="1.6.47.1",
+    version="1.6.48.1",
     keywords=("numkey E.164 shortcode lvn did encoding"),
     description="NumKey Bindings for Python",
     long_description=read("../README.md"),
